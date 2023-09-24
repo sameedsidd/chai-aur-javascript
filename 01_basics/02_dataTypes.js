15f820c349ffbf28d9935a8c827be7d5babadaae
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-09-24 12:43:03 +0500

    learn datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version
+// https://tc39.es/ecma262/#sec-number-and-dates
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+// console.log(3 + 
+     
+//     3); // code readability should be hight
+
+    // console.log("Sameed");
+
+    let name = "Sameed"
+    let age = 22
+    let isLoggedIn = false
+    let state;
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // bolean => true/false
+    // null => stand alone value
+    // undefined =>
+    // symbol => unique
+
+    // object
+
+    console.log(typeof null);
+    console.log(typeof undefined);
