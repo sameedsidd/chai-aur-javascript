e1a8a9a9bad9b452d213950efbcc182e4e84b75f
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-09-25 11:45:31 +0500

    stack and heap memory

--- 01_basics/dataTypeSummary.js
+++ 01_basics/dataTypeSummary.js
@@ -30,13 +30,38 @@ const info = {
 }
 
 const myFunction = function() {
-    console.log("Hello World");
+    // console.log("Hello World");
 }
 
-console.log(typeof(heros));
-console.log(typeof(info));
-console.log(typeof(myFunction));
+// console.log(typeof(heros));
+// console.log(typeof(info));
+// console.log(typeof(myFunction));
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
 
+// +++++++++++++++++++++++++++++++++++++
+
+// Primitive DataTypes use (Stack Memory)
+// * stact memory gives you copy to change data
+
+
+// Refernece DataTypes use (Heap Memory)
+// * heap memory gives you direct reference
+
+let myYoutubName = "sameedsiddiquidotcom"
+let anotherName = myYoutubName
+anotherName = "chaiaurcode"
+
+console.log(myYoutubName);
+console.log(anotherName);
+
+let userOne = {
+    id: 122,
+    email: "a@google.com"
+}
+ let userTwo = userOne
+userTwo.email = "b@gmail.com"
+
+ console.log(userOne.email);
+ console.log(userTwo.email);
