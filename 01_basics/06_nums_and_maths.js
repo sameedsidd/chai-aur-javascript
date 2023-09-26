abbfa4aed3e24460a24e00cf1dbae15a472d19f3
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-09-26 18:31:16 +0500

    numbers and maths methods

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,42 @@
+console.log("==================================================");
+
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toFixed(2))
+// console.log(balance.toString());
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString('en-PK'));
+
+// ++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++ //
+
+// console.log(Math);
+// console.log(Math.abs(-4)) // absolute value
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3));
+// console.log(Math.min(2, 3, 4, 5, 5, 0));
+// console.log(Math.max(2, 3, 4, 5, 5, 0));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+console.log("==================================================");
