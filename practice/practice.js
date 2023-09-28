d4808004f9ba99de50a0dedb6b8494598dd5fabd
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-09-28 19:55:31 +0500

    number-maths

--- practice/practice.js
+++ practice/practice.js
@@ -195,6 +195,23 @@ const url = "https://www.google.com"
 
 
 
+// +++++++++++++++ number and maths +++++++++++++ //
+
+const score = 100
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toFixed(2));
+console.log(balance.toString().charAt(2));
+
+const otherNumber = 123.432
+console.log(otherNumber.toPrecision(3));
+
+const hundreds = 10000
+console.log(hundreds.toLocaleString('en-PK'));
+
+// ************* maths *********************
 
 
 
