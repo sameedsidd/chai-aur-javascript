9433195c359f82177e38bc26e9b96dcd846a082a
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-09-28 11:49:16 +0500

    practice-datatype

--- practice/practice.js
+++ practice/practice.js
@@ -2,16 +2,16 @@
 // var, let and const
 
 console.log("=============== Result =================");
-const accountId = 1288479
-let accountEmail = "a@google.com"
-var accountPassword = "232323"
-let accountState;
-accountCity = "Karachi"
+// const accountId = 1288479
+// let accountEmail = "a@google.com"
+// var accountPassword = "232323"
+// let accountState;
+// accountCity = "Karachi"
 
 // accountId = "19347" // not allowed to reassign constant variable
-accountEmail = "b@gmail.com"
-accountPassword = "131313"
-accountCity = "Lahore"
+// accountEmail = "b@gmail.com"
+// accountPassword = "131313"
+// accountCity = "Lahore"
 
 
 /*
@@ -20,5 +20,63 @@ because of issue in block scope and functional scope
 */
 
 // console.log(accountEmail);
-console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
+// console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
+
+
+
+
+// +++++++++++++++ data-types +++++++++++++ //
+
+"use strict" // treat all JS code as newer version
+
+// alert("Sameed") // we are using nodejs, not browser
+
+// console.log(2 
+//     + 
+//     3); // code should be readable 
+
+const myName = "Sameed Siddiqui"
+let myAge = 22
+let isLoggedIn = false
+let state;
+
+/*
+primitive data type (stack memory used)
+number =>> 2 to power 53
+bigInt
+boolean =>> true/false
+null =>> stand-alone value (type as object)
+undefined =>> undefined
+symbol =>> unique value
+
+non-primitive data type (heap memory used)
+array
+object
+function
+*/
+
+// console.log(typeof null);
+// console.log(typeof undefined);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 console.log("========================================");
