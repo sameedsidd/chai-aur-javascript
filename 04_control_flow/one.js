4460c8f23fe8b24ac01fe4001041448fddde3ad7
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-10-09 10:29:41 +0500

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,68 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 41
+// if (temperature < 50) {
+//     console.log("temperature is less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+
+
+// let score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+
+// let balance = 1000
+// // if (balance > 500) console.log("test"), console.log("test2"); // don't do that
+
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// }
+// else if (balance < 750) {
+//     console.log("less than 750");
+// }
+// else if (balance < 900) {
+//     console.log("less than 900");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (isUserLoggedIn && debitCard && 2=="2") {
+    console.log("Allow to buy courses");
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// <, >, <=, >=, ==, !=, ===, ==!
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,19 @@
+const month = "mar"
+switch (month) {
+    case "jan":
+        console.log("jan");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("mar");
+        // break;
+    case "apr":
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
