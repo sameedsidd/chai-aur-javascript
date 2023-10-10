8448de76f6e0cec5f6e546deeeacc339242895c2
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2023-10-10 16:47:20 +0500

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,51 @@
+// for loop
+for (let i = 0; i < 10; i++) {
+    const element = i
+    if (element == 5) {
+        // console.log("5 is the best number");
+    }
+    // console.log(element);
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(`Outer loop value: ${i}`);
+    for (let j = 0; j <= 10; j++) {
+        // console.log(`Inner loop value: ${j} and outer loop value: ${i}`);
+        // console.log(`${i} * ${j} = ${(i*j)}`);
+    }
+}
+
+
+let myArray = ["flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+
+for (let index = 0; index <= 10; index++) {
+    if (index == 5) {
+        // console.log(`5 is detected`);
+        break;
+    }
+    // console.log(index);
+}
+
+
+
+
+for (let index = 0; index < 10; index++) {
+    if (index == 5) {
+        console.log(`5 is detected`);
+        continue;
+    }
+    console.log(index);
+}
+
+
+
+
+
+
+
+    
