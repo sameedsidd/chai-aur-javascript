5a78a05746e72c7fb76e498dff2b15da2620e871
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-11 19:33:11 +0500

    array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,42 @@
+// array
+
+const myArr = [1, 2, 3, 4, 5];
+const myHeros = ["Superman", "Batman", "Flash"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+
+// console.log(myArr[2]);
+
+// array methods
+
+myArr.push(6)
+myArr.push(7)
+
+myArr.pop()
+myArr.pop()
+myArr.pop()
+
+myArr.unshift(0)
+myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(4));
+// console.log(myArr.indexOf(2));
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// slice, splice
+
+console.log("A:", myArr);
+
+const my1 = myArr.slice(1, 3)
+console.log(my1);
+
+console.log("B:", myArr);
+
+const my2 = myArr.splice(1, 3)
+console.log(my2);
+
+console.log("C:", myArr);
