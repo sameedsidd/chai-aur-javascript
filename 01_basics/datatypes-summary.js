507712df634a7667964c83f33fc9a42483c0fe98
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-11 16:05:51 +0500

    stack and heap

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -17,21 +17,40 @@ const anotherId = Symbol("123");
 const bigNumber = 12233227827632827327n;
 // console.log(typeof bigNumber);
 
-
 // Reference (Non-primitive)
 
 // Array, Object, Function
 
-const heros = ["Superman", "Batman", "Spider"]
+const heros = ["Superman", "Batman", "Spider"];
 const info = {
-    name: "Sameed",
-    age: 22
-}
-const myFuntion = function() {
-    console.log("Hello Sam!");
-}
+  name: "Sameed",
+  age: 22,
+};
+const myFuntion = function () {
+  console.log("Hello Sam!");
+};
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
 
+// ********************************************************
 
+// Stack (Primitive), Heap (Non-Primotive)
 
+let myYoutubeName = "histeshchoudharydotcom";
+
+let anothername = myYoutubeName;
+anothername = "chaiaurcode";
+
+// console.log(myYoutubeName);
+// console.log(anothername);
+
+let userOne = {
+  email: "a@gmail.com",
+  id: 222,
+};
+
+let userTwo = userOne;
+userTwo.email = "b@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
