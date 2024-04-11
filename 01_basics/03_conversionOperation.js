6132fca7086d1707fa7858e0c901b8bb5d838897
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-11 14:03:53 +0500

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = false
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Sameed"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false; "Sameed" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+
+
+
+
+
+
