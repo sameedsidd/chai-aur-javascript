d7d6e85dbc8dbc19b0c2caf7b663e0fc0ab3dcf9
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-11 15:31:18 +0500

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.0;
+
+const isLoggedIn = true;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+const bigNumber = 12233227827632827327n;
+// console.log(typeof bigNumber);
+
+
+// Reference (Non-primitive)
+
+// Array, Object, Function
+
+const heros = ["Superman", "Batman", "Spider"]
+const info = {
+    name: "Sameed",
+    age: 22
+}
+const myFuntion = function() {
+    console.log("Hello Sam!");
+}
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
