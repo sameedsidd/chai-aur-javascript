8bb109aa744e7d643462c2c1d1fbb9155635ad1f
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-13 11:44:57 +0500

    object functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -24,11 +24,39 @@ const result = addTwoNumber(10, 34);
 // console.log(result);
 
 function loginUserMessage(username = "Sam") {
-    if (!username) {
-        console.log("Please enter your name");
-        return
-    }
-    return `${username} just logged in.`
+  if (!username) {
+    console.log("Please enter your name");
+    return;
+  }
+  return `${username} just logged in.`;
 }
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1;
+}
+
+// console.log(calculateCartPrice(200, 300, 400, 500, 20000));
+
+const user = {
+  username: "Sameed",
+  id: 12233,
+};
+
+function handleObject(anyObject) {
+  console.log(`Username is ${anyObject.username} Id is ${anyObject.id}`);
+}
+
+// handleObject(user)
+// handleObject({
+//   username: "Sam",
+//   id: 22,
+// });
+
+const myNewArray = [100, 200, 300, 400, 500];
+function returnSecondValue(getArray) {
+  return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 300, 400, 500]));
