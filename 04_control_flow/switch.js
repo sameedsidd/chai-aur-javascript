24248c89d3f1ac5adc93f20c450c431ce21825c3
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-14 21:08:07 +0500

    control flows

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,32 @@
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("Jan");
+    break;
+  case 2:
+    console.log("Feb");
+    break;
+  case 3:
+    console.log("Mar");
+    break;
+  case 4:
+    console.log("Apr");
+    break;
+
+  default:
+    console.log("No match");
+    break;
+}
+
+// ==============================
+
+
+const months = "jan"
+switch (months) {
+    case "jan":
+        console.log("Matched");
+        break;
+
+    default:
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,43 @@
+const userEmail = []
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+// falsy values
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+// truthy values
+// "0", 'false', "", [], {}, function(){}
+
+
+if (userEmail.length == 0) {
+    console.log("Array is empty");
+}
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj)) {
+    console.log("Object is empty");
+}
+
+// false == 0 : true
+// false == '' : true
+// 0 == '' : true
+
+
+// Nullish Coalescing Operator (??): null and undefined
+
+let val1 = 5;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+// val1 = null ?? 10 ?? 20
+// console.log(val1);
+
+// Ternary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
