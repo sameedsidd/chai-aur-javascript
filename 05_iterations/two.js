a8515434ebed90ebbef8496fdf30befa46ccaba1
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2024-04-21 11:04:48 +0500

    do and while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`value of index is ${index}`);
+//   index = index + 2;
+// }
+
+let myArr = ["A", "B", "C"]
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(myArr[arr]);
+    arr = arr + 1
+}
+
+
+
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score < 10);
